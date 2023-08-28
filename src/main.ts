0b5092f299d8307af29904f81dc5643b7dde1996
eomgerm <fishbread00@gmail.com>
2023-08-29 02:24:17 +0900

    Feat: Swagger, Pino(Logger) 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
