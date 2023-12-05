7b4e0525ef2d092fcbdbd303ef96d75f4ccf6814
eomgerm <fishbread00@gmail.com>
2023-12-05 19:54:49 +0900

    📝 [Docs]: 카카오 로그인 명세서 추가

--- src/auth/dto/kakao-oauth-request.dto.ts
+++ src/auth/dto/kakao-oauth-request.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class KakaoOAuthRequest {
   @IsNotEmpty({ message: '액세스 토큰은 필수입니다.' })
   @IsString({ message: '문자열 형태가 아닙니다.' })
+  @ApiProperty({ description: '카카오 Access Token' })
   readonly accessToken: string;
 }
