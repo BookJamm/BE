bfdfb352c93e5bdb4261e8972bebcb57013cf2c2
eomgerm <fishbread00@gmail.com>
2023-12-05 20:15:56 +0900

    🔥 [Update]: 불필요한 API 삭제

--- src/activity/activity.controller.ts
+++ src/activity/activity.controller.ts
@@ -1,18 +1,10 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
-import {
-  ApiBearerAuth,
-  ApiOkResponse,
-  ApiOperation,
-  ApiParam,
-  ApiQuery,
-  ApiTags,
-} from '@nestjs/swagger';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guard/auth.guard';
 import { BaseResponse } from 'src/global/base/base-response';
 import { ExtractPayload } from 'src/global/decorator/extract-payload.decorator';
 import { ActivityService } from './activity.service';
 import { ActivityDetailResponse } from './dto/activity-detail-response.dto';
-import { ActivityReviewListResponse } from './dto/activity-review-list-response.dto';
 
 @Controller('api/activities')
 @UseGuards(JwtAuthGuard)
@@ -33,22 +25,5 @@ export class ActivityController {
     @Param('activityId') activityId: number,
   ): Promise<BaseResponse<ActivityDetailResponse>> {
     return new BaseResponse(await this.activityService.getActivityDetail(userId, activityId));
-  }
-
-  @Get(':activityId/reviews')
-  @ApiOperation({
-    summary: '활동 리뷰 조회',
-  })
-  @ApiParam({ name: 'activityId', description: '리뷰를 조회할 활동 아이디' })
-  @ApiQuery({ name: 'last', description: '마지막으로 조회한 리뷰 아이디, 페이징 용' })
-  @ApiOkResponse({ type: [ActivityReviewListResponse] })
-  async findActivityReviews(
-    @ExtractPayload() userId: number,
-    @Param('activityId') activityId: number,
-    @Query('last') last: number,
-  ) {
-    return new BaseResponse(
-      await this.activityService.findActivityReviews(userId, activityId, last),
-    );
   }
 }
