dbb39f2d44cd0c3aef98d0c841002fb1feeedc5e
eomgerm <fishbread00@gmail.com>
2023-12-27 19:29:52 +0900

    ♻️ [Rename]: PlaceReivew로 이름 통일

--- src/place/dto/response/create-place-review-response.dto.ts
+++ src/place/dto/response/create-place-review-response.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateReviewResponse {
+export class CreatePlaceReviewResponse {
   @ApiProperty({ description: '생성된 리뷰의 아이디', example: 1 })
   readonly reviewId: number;
 }
